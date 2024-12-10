465e3356c40621f3a46e2fe13e7c336b4e3fda08
osariusz <osariusz@interia.pl>
2024-12-10 10:55:17 +0100

    Cleanup

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ import discord
 from discord.ext import commands
 from bot import Bot
 
-# Load environment variables from .env file
 load_dotenv()
-
-# Create an instance of the bot with proper intents
-intents = discord.Intents.all()  # Modify intents if needed
+intents = discord.Intents.all()
 bot = Bot(intents=intents)
 
-# Run the bot
 if __name__ == "__main__":
-    # Load token from the environment
     bot.load_extension('cogs.feudal_cog')
     TOKEN: str = os.getenv("DISCORD_TOKEN", "")
     if not TOKEN:
