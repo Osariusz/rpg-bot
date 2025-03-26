4682db42a15452bab390eb32349bf19562c46147
osariusz <osariusz@interia.pl>
2025-03-26 21:37:45 +0100

    format

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ class Bot(commands.Bot):
         if(len(message_content_lines) > 0 and len(message_content_lines[0]) > 0):
             name = message_content_lines[0]
         date_string = datetime.now().strftime("%d.%m.%Y")
-        name = date_string + name
+        name =  f"{date_string} {name}"
         new_thread: discord.Thread = await message.create_thread(name=name)
         if(thread_channel.ping_id != None):
             await new_thread.send(f"<@{thread_channel.ping_id}>", mention_author=False)
